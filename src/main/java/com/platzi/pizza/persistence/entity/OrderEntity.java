ca9c250e00e237951a413b288353be327b563932
Brayan Rodallega <thebrayan1a1@outlook.com>
2023-07-06 23:55:46 -0500

    :sparkles: [ADD] Entities

---
+++ src/main/java/com/platzi/pizza/persistence/entity/OrderEntity.java
@@ -0,0 +1,45 @@
+package com.platzi.pizza.persistence.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "pizza_order")
+@Getter
+@Setter
+@NoArgsConstructor
+public class OrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_order", nullable = false)
+    private Integer idOrder;
+
+    @Column(name = "id_customer", nullable = false, length = 15)
+    private String idCustomer;
+
+    @Column(nullable = false, columnDefinition = "DATETIME")
+    private LocalDateTime date;
+
+    @Column(nullable = false, columnDefinition = "Decimal(6,2)")
+    private Double total;
+
+    @Column(nullable = false, columnDefinition = "CHAR(1)")
+    private String method;
+
+    @Column(name = "additional_notes", length = 200)
+    private String additionalNotes;
+
+    @OneToOne
+    @JoinColumn(name = "id_customer", referencedColumnName = "id_customer", insertable = false, updatable = false)
+    private CustomerEntity customer;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItemEntity> items;
+}
---
+++ src/main/java/com/platzi/pizza/persistence/entity/OrderItemEntity.java
@@ -0,0 +1,40 @@
+package com.platzi.pizza.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "order_item")
+@Getter
+@Setter
+@NoArgsConstructor
+@IdClass(OrderItemId.class)
+public class OrderItemEntity {
+
+    @Id
+    @Column(name = "id_order", nullable = false)
+    private Integer idOrder;
+
+    @Id
+    @Column(name = "id_item", nullable = false)
+    private Integer idItem;
+
+    @Column(name = "id_pizza", nullable = false)
+    private Integer idPizza;
+
+    @Column(nullable = false, columnDefinition = "Decimal(2,1)")
+    private Double quantity;
+
+    @Column(nullable = false, columnDefinition = "Decimal(5,2)")
+    private Double price;
+
+    @ManyToOne
+    @JoinColumn(name = "id_order", referencedColumnName = "id_order", insertable = false, updatable = false)
+    private OrderEntity order;
+
+    @OneToOne
+    @JoinColumn(name = "id_pizza", referencedColumnName = "id_pizza", insertable = false, updatable = false)
+    private PizzaEntity pizza;
+}
