d5adc6038a8eec1f0da40f755241e50e3035d284
Brayan Rodallega <thebrayan1a1@outlook.com>
2023-07-07 23:58:01 -0500

    :sparkles: [ADD] Customer Service

---
+++ src/main/java/com/platzi/pizza/service/CustomerService.java
@@ -0,0 +1,22 @@
+package com.platzi.pizza.service;
+
+import com.platzi.pizza.persistence.entity.CustomerEntity;
+import com.platzi.pizza.persistence.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public CustomerEntity findByPhone(String phone) {
+        return customerRepository.findByPhone(phone);
+    }
+
+}
