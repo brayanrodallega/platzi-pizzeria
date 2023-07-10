c9b6a63719193702af6da60f32f47e391513d76c
Brayan Rodallega <thebrayan1a1@outlook.com>
2023-07-10 02:27:14 -0500

    :wrench: [UPDATE] Permit CORS

--- src/main/java/com/platzi/pizza/web/config/CorsConfig.java
+++ src/main/java/com/platzi/pizza/web/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
 
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5500/"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
 
